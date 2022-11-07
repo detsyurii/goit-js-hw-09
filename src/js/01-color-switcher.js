26f6636f05f6d44e6a08ada5836fa929fed8f8c8
detsyurii <dets.yurii@gmail.com>
2022-11-07 14:51:36 +0200

    1 task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtnRef = document.querySelector('button[data-start]');
+const stopBtnRef = document.querySelector('button[data-stop]');
+
+let colorChangeIntervalId = null;
+const INTERVAL_DELAY = 1000;
+
+startBtnRef.addEventListener('click', onStartBtnClick);
+stopBtnRef.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick(evt) {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorChangeIntervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, INTERVAL_DELAY);
+
+  startBtnRef.disabled = true;
+}
+
+function onStopBtnClick() {
+  clearInterval(colorChangeIntervalId);
+  startBtnRef.disabled = false;
+}
